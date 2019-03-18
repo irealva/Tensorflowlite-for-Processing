f6abe65bfbb848c1f456d72f8a00cfa93268c066
Irene Alvarado <ire.alvarado@gmail.com>
2019-03-18 08:36:04 -0400

    Adding image classification code

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 1. Refer to the tools prerequisites for the[Processing Android Library Template](https://github.com/processing/processing-android-library-template)
 
-2. Run `gradle dist` to build the library
+2. Add a local.properties file with the path to your sdk
 
-3. Run `gradle clean` to cleanup the build
+3. Run `gradle dist` to build the library
+
+4. Run `gradle clean` to cleanup the build
+
+## Using the Library
+
+note: you need a special build of Processing for Android
+
+
+
+## To Do
+
+* Update to tensorflow 1.13.0
+* Image classification: check for other classes of models
+* Implement video image classification
--- src/main/java/tensorflowlite/Tflite.java
+++ src/main/java/tensorflowlite/Tflite.java
@@ -1,30 +1,144 @@
 /**
- * tflite
- * ##library.sentence##
- * ##library.url##
+ * Copyright 2019 Google LLC
  *
- * Copyright ##copyright## ##author##
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *     https://www.apache.org/licenses/LICENSE-2.0
  * 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA  02111-1307  USA
- * 
- * @author      Irene Alvarado
- * @modified    February 8, 2019
- * @version     0.1.1
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+package tensorflowlite;
+
+//import tensorflowlite.*;
+import processing.core.PApplet;
+
+import org.tensorflow.lite.*;
+import android.app.Activity;
+import java.util.List;
+import java.io.*;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// Todo extract byte buffer allocation
+// Figure out naming of variables
+
+public class Tflite {
+  PApplet parent;
+  Activity activity;
+  List<String> labelList;
+  Interpreter interpreter;
+  Classifier classifier;
+
+  /** Can be set by users. */
+  int INPUT_SIZE;
+  boolean QUANT = false;
+
+  // show results
+  int RESULTS_TO_SHOW = 3;
+
+  public Tflite(PApplet parent) {
+    this.parent = parent;
+    this.activity = parent.getActivity();
+  }
+
+  public void setInputSize(int size) {
+    INPUT_SIZE = size;
+  }
+
+  public void isQuantModel(boolean quant) {
+    QUANT = quant;
+  }
+
+  public Bitmap loadImage(String path) {
+    // How to print all images in assets folder:
+    //String[] test = surface.getAssets().list("");
+    //System.out.println(Arrays.toString(test));
+
+    Bitmap loadedImage = null;
+
+    try {
+      //InputStream is = surface.getAssets().open(path);
+      InputStream is = this.activity.getAssets().open(path);
+      loadedImage = loadFrom(is);
+      is.close();
+    }
+    catch (IOException e) {
+    }
+
+    return loadedImage;
+  }
+
+  Bitmap loadFrom(InputStream is) { 
+    try {
+      return BitmapFactory.decodeStream(is);
+    } 
+    catch (Exception e) {
+      System.out.println(e);
+      return null;
+    }
+  }
+
+  public Bitmap resizeImage(Bitmap bitmap) {
+    return Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+  }
+
+  public Bitmap cropCenterImage(Bitmap bitmap) {
+    Bitmap result;
+    int w = bitmap.getWidth();
+    int h = bitmap.getHeight();
+
+    if (w == h) {
+      return bitmap;
+    }
+
+    if (w > h) {
+      result = Bitmap.createBitmap(bitmap, w/2 - h/2, 0, h, h);
+    } else {
+      result = Bitmap.createBitmap(bitmap, 0, h/2 - w/2, w, w);
+    }
+
+    return result;
+  }
+
+  // TODO: should probably catch when folks are using quant vs not spits an IllegalArgumentException
+  public void loadModel(String modelPath, String labelPath) {
+    try {
+      if (QUANT) {
+        this.classifier = new ClassifierQuantizedMobileNet(activity, modelPath, labelPath, INPUT_SIZE, INPUT_SIZE);
+      } else {
+        this.classifier = new ClassifierFloatMobileNet(this.activity, modelPath, labelPath, INPUT_SIZE, INPUT_SIZE);
+      }
+    } 
+    catch (IOException e) { 
+      System.out.println("Failed to load classifier");
+      System.out.println(e.getMessage());
+      //return;
+    }
+  }
+
+  public void setMaxResults(int maxResults) {
+    this.classifier.setMaxResults(maxResults);
+  }
+
+  public String recognizeImage(Bitmap img) {
+    List<Classifier.Recognition> results = this.classifier.recognizeImage(img);
+    return results.toString();
+  }
+}
+
+
+
+/*
 package tensorflowlite;
 
 import processing.core.*;
@@ -34,18 +148,6 @@ import java.nio.MappedByteBuffer;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
-
-/**
- * This is a template class and can be used to start a new processing library or tool.
- * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own library or tool naming convention.
- * 
- <!-- * @example Hello  -->
- * 
- * (the tag @example followed by the name of an example included in folder 'examples' will
- * automatically include the example in the javadoc.)
- *
- */
 
 public class Tflite {
 	
@@ -79,18 +181,19 @@ public class Tflite {
 	    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
 	}
 	
-	/*
-	public String sayHello() {
-		return "hello library.";
-	}
+	// public String sayHello() {
+	// 	return "hello library.";
+	// }
 
-	public void setVariable(int theA, int theB) {
-		myVariable = theA + theB;
-	}
+	// public void setVariable(int theA, int theB) {
+	// 	myVariable = theA + theB;
+	// }
 
-	public int getVariable() {
-		return myVariable;
-	}
-	*/
+	// public int getVariable() {
+	// 	return myVariable;
+	// }
 }
+	*/
+
+
 
